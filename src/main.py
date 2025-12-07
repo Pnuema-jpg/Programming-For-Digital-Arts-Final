7562ca35bf877e5bd1abd3ba987eac24feb4a81b
Ian Pham <notianpham@gmail.com>
2025-12-07 01:49:58 -0600

    adds background and background image

---
+++ src/main.py
@@ -0,0 +1,23 @@
+#  make a top down map with a moveable player using pygame
+import pygame
+import sys
+import random
+from sys import exit
+from pygame.locals import QUIT
+import math
+
+
+pygame.init()
+# create window
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Photon Edge")
+clock = pygame.time.Clock()
+
+background = pygame.image.load("background.png").convert()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.blit(background, (0, 0))
