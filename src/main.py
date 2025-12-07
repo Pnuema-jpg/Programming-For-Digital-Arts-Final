d17860c8ca048bdd8b1e26f5b4fbc270cc1b57f0
Ian Pham <notianpham@gmail.com>
2025-12-07 02:33:03 -0600

    can move on a map

--- src/main.py
+++ src/main.py
@@ -10,24 +10,48 @@ import math
 
 
 
-
-
-
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        
-
-
-
-pygame.init()
+pygame.init()     
 # create window
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Photon Edge")
 clock = pygame.time.Clock()
-
 background = pygame.image.load("background.png").convert()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("player.png").convert_alpha()
+        self.pos = pygame.math.Vector2(400, 300)
+        self.speed = 5
+    def user_input(self):
+        self.velocity_x = 0
+        self.velocity_y = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.velocity_y = -self.speed
+            self.velocity_x = 0
+        if keys[pygame.K_s]:
+            self.velocity_y = self.speed
+            self.velocity_x = 0
+        if keys[pygame.K_d]:
+            self.velocity_x = self.speed
+            self.velocity_y = 0
+        if keys[pygame.K_a]:
+            self.velocity_x = -self.speed
+            self.velocity_y = 0
+    def move(self):
+        self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
+    def update(self):
+        self.user_input()
+        self.move()
+    
+
+player1 = Player()
+        
+
+
+
 
 while True:
     for event in pygame.event.get():
@@ -35,5 +59,7 @@ while True:
             pygame.quit()
             sys.exit()
     screen.blit(background, (0, 0))
+    screen.blit(player1.image, player1.pos)
+    player1.update()
     pygame.display.update()
     clock.tick(60)
