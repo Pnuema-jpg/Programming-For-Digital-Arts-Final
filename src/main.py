56cb9ccff70c404fd815834da52133326e621985
Ian Pham <notianpham@gmail.com>
2025-12-08 00:49:03 -0600

    idk how much but im working on the sword beam like from zelda

--- src/main.py
+++ src/main.py
@@ -27,6 +27,8 @@ class Player(pygame.sprite.Sprite):
         self.hitbox = self.image.get_rect(center = self.pos)
         self.rect = self.hitbox.copy()
         self.speed = 5
+        self.swordbeam_cooldown = 0
+        self.swordbeam = False
 
     def player_rotate(self):
         self.mouse_pos = pygame.mouse.get_pos()
@@ -52,6 +54,18 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_a]:
             self.velocity_x = -self.speed
             self.velocity_y = 0
+
+        if pygame.mouse.get_pressed() == (1, 0, 0):
+            self.swordbeam() == True
+            self.swordbeamexists = True
+        else :
+            self.swordbeam() == False
+    def swordbeamexists(self):
+        if self.swordbeam_cooldown == 0:
+            self.swordbeam_cooldown = 20
+            beam_spawn = self.pos
+            self.beam = Beam(beam_spawn, )
+
     def move(self):
         self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
         self.hitbox.center = (round(self.pos.x), round(self.pos.y))
@@ -60,10 +74,39 @@ class Player(pygame.sprite.Sprite):
         self.user_input()
         self.move()
         self.player_rotate()
+        if self.swordbeam_cooldown > 0:
+            self.swordbeam_cooldown -= 1
 
+class Beam(pygame.sprite.Sprite):
+    def __init__(self, pos, angle):
+        super().__init__()
+        self.image = pygame.image.load("Beam.png").convert_alpha()
+        self.image = pygame.transform.rotozoom(self.image,0,1)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+        self.x = x
+        self.y = y
+
+        self.speed = 10
+        self.angle = angle
+        self.velocity = pygame.math.Vector2(self.speed, 0).rotate(-self.angle)
+    def beam_movement(self):
+        self.x += self.velocity.x
+        self.y += self.velocity.y
+
+        self.rect.x = int(self.x)
+        self.rect.y = int(self.y)
+
+
+    def update(self):
+        self.beam_movement()
+
+    
 player1 = Player()
         
-
+all_sprites = pygame.sprite.Group()
+beam_group = pygame.sprite.Group()
+all_sprites.add(player1)
 
 
 
